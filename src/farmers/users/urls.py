3103044d02509345a412704edc93abf0d78f2907
manulangat <emmanuelthedeveloper@gmail.com>
2020-05-31 22:09:10 +0300

    bg-removing bugs-backend

--- src/farmers/users/urls.py
+++ src/farmers/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path,include
+from django.conf.urls import url
 from .api import RegisterAPI,LoginAPI,UserAPI,ActivateToken
 from .views import activate_account
 from knox import views as knox_views
@@ -9,4 +10,6 @@ urlpatterns = [
     path('v1/user/',UserAPI.as_view(),name="user_api"),
     path('v1/activate/<uidb64>/<token>',activate_account,name="activate"),
     path('v1/logout/',knox_views.LogoutView.as_view(),name="logout"),
+    path('api/auth/',include('knox.urls')),
+    url(r'^api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
