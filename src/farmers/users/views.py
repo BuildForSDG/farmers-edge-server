b6abe1b591f94f40e8637a45d9a8ae0b5a492342
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-02 20:48:44 +0300

    bg-fixing view bug-backend

--- src/farmers/users/views.py
+++ src/farmers/users/views.py
@@ -6,7 +6,7 @@ from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .token_generator import account_activation_token
 # Create your views here.
-def activate_account(request, uidb64, token):
+def activate_account(request,  uidb64, token):
     try:
         uid = force_bytes(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
@@ -21,12 +21,23 @@ def activate_account(request, uidb64, token):
     else:
         return HttpResponse('Activation link is invalid!')
 def confirm_update(request,uidb64,token):
-    if request.method == 'POST':
-        print('request.data')
-        return HttpResponse("got it")
-    else:
-        print("hey")
-        return HttpResponse("p")
+    if 'password' in request.GET and request.GET["password"]:
+        password = request.GET.get('password')
+        print(password)
+        try:
+            uid = force_bytes(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+            print(user,token)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(user, token):
+            print("usr",user)
+            user.set_password(password)
+            user.save()
+            return HttpResponse({"you have reset your password successfully"})
+        else:
+            return HttpResponse({"Invalid token"})
+    return render(request,'change/pass_change.html')
         # return Response("hey")
 # def reset_password(request, uidb64, token):
 #     try:
